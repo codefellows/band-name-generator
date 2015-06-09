a1756ba982e3a1427a553e2d627e925841de3e30
bentongreen <bentongreen076@gmail.com>
2015-06-08 20:41:43 -0700

    randomness generator server

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 3000;
+
+var strings = ["amber", "apple", "paint", "ChimpanzeeCat"];
+var adjectives = ["purple", "bumpy", "savory"];
+app.get("/", function(req, res) {
+	var string = strings[Math.floor(Math.random()*strings.length)];
+	res.send(string);
+});
+
+app.get("/adjectives", function(req, res){
+	var randomIndex = Math.floor(Math.random() * adjectives.length);
+	res.json({"word": adjectives[randomIndex]});	
+});
+
+app.listen(port, function(){
+	console.log('server started on port ' + port);
+});
