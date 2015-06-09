ef53b2104d8ad21c8b8f98db2b98366cfc81485d
bentongreen <bentongreen076@gmail.com>
2015-06-08 20:57:20 -0700

    Added index.html and newest server.js

--- server.js
+++ server.js
@@ -4,12 +4,16 @@ var port = process.env.PORT || 3000;
 
 var strings = ["amber", "apple", "paint", "ChimpanzeeCat"];
 var adjectives = ["purple", "bumpy", "savory"];
-app.use(express.static(__dirname + "/app/"));
+
+
 app.get("/", function(req, res) {
-	var string = strings[Math.floor(Math.random()*strings.length)];
-	res.send(string);
+	//var string = strings[Math.floor(Math.random()*strings.length)];
+	//res.send(string);
+	res.sendFile(__dirname + "/app/index.html");
+
 });
 
+app.use(express.static(__dirname + "/app/"));
 app.get("/adjectives", function(req, res){
 	var randomIndex = Math.floor(Math.random() * adjectives.length);
 	res.json({"word": adjectives[randomIndex]});	
