bb985d952cf96c6d31e627ce0cebfcb6886a2c70
bentongreen <bentongreen076@gmail.com>
2015-06-10 13:29:32 -0700

    work on 6/10 at 1:30

--- server.js
+++ server.js
@@ -6,14 +6,16 @@ var strings = ["amber", "apple", "paint", "ChimpanzeeCat"];
 var adjectives = ["purple", "bumpy", "savory"];
 
 
-app.get("/", function(req, res) {
+/*app.get("/", function(req, res) {
 	//var string = strings[Math.floor(Math.random()*strings.length)];
 	//res.send(string);
+	console.log(__dirname);
 	res.sendFile(__dirname + "/app/index.html");
 
-});
+});*/
 
 app.use(express.static(__dirname + "/app/"));
+
 app.get("/adjectives", function(req, res){
 	var randomIndex = Math.floor(Math.random() * adjectives.length);
 	res.json({"word": adjectives[randomIndex]});	
