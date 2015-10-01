d5c89e17f7d07a9f95e41e25e4f52d3fe9630150
NoahNim <nmedoff123@gmail.com>
2015-09-30 20:32:31 -0700

    server.js made

---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 3000;
+
+function Adjective(){
+  this.happy = true;
+  this.evil = true;
+  this.good = true;
+  this.sad = true;
+  this.mad = true;
+  this.explosive = true;
+  this.fuzzy = true;
+  this.pokemon = true;
+}
+
+var adjective = new Adjective();
+
+function getRandomWord (object){
+  var propArray = Object.keys(object);
+  var randomProp = propArray[Math.floor(Math.random()*propArray.length)];
+  return {word: randomProp};
+}
+
+getRandomWord(adjective);
+
+app.get('/adjective', function(req, res){
+  res.json(getRandomWord(adjective));
+});
+
+app.get("/", function(req, res){
+  res.send("hello universe!");
+});
+
+app.listen(port, function(){
+  console.log('server started on port, access at localhost:' + port);
+  console.log(getRandomWord(adjective))
+});
