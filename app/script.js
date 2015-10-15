3080f967e32d65e32ee0d5c30d940f5bee49f018
Jack Sneed <sneed.jack@gmail.com>
2015-10-15 15:44:51 -0700

    Update nouns and verbs.

---
+++ app/script.js
@@ -0,0 +1,57 @@
+'use strict';
+
+$(function() {
+
+  $("button").click(function() {
+    $.get('adjective', function(response) {
+      var adjective = response.word;
+      $("#adjective").text(adjective);
+    });
+
+    $.get('verb', function(response) {
+      var verb = response.word;
+      $("#verb").text(verb);
+    });
+
+    $.get('noun', function(response) {
+      var noun = response.word;
+      $("#noun").text(noun);
+    });
+
+  });
+
+  $("#submitWords").on("submit", function(e) {
+    e.preventDefault();
+
+    var adjective = $("input[name=adjective]").val();
+    var noun = $("input[name=noun]").val();
+    var verb = $("input[name=verb]").val();
+    var adjPost;
+
+    if (adjective) {
+      adjPost = {word: adjective};
+      $.post("adjective", adjPost, function(response) {
+        var adjectiveRes = response.msg;
+        $("#adjectiveRes").text(adjectiveRes);
+      });
+    }
+
+    if (noun) {
+      adjPost = {word: noun};
+      $.post("noun", adjPost, function(response) {
+        var nounRes = response.msg;
+        $("#nounRes").text(nounRes);
+      });
+    }
+
+    if (verb) {
+      adjPost = {word: verb};
+      $.post("verb", adjPost, function(response) {
+        var verbRes = response.msg;
+        $("#verbRes").text(verbRes);
+      });
+    }
+
+  });
+
+});
--- server.js
+++ server.js
@@ -1,33 +1,68 @@
-var express = require("express");
-var app = express();
-var port = process.env.PORT || 3000;
+'use strict';
 
-function Adjective(){
-    this.happy = true;
-    this.sad = true;
-    this.sappy = true;
-    this.blue = true;
-};
+var express = require('express');
+var app = express();
+var bodyparser = require('body-parser');
+var port = process.env.PORT || 3000;
+var Adjective = require('./lib/adjective.js');
+var Noun = require('./lib/noun.js');
+var Verb = require('./lib/verb.js')
+var getRandomWord = require('./lib/getRandomWord.js');
 
 var adjective = new Adjective();
 
-function getRandomWord(object) {
-  var propArray = Object.keys(object);
-  var randomProp = propArray[Math.floor(Math.random() * propArray.length)];
-  return {word: randomProp};
+var verb = new Verb();
+
+var noun = new Noun();
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({
+  extended: true
+}));
+
+app.use(express.static(__dirname + '/app/'));
+
+function postWord (word, wordObject) {
+  if (wordObject.hasOwnProperty(word)) {
+    return {msg: 'We already have your awesome word, ' + word + ', in our list.'};
+  }
+
+  wordObject[word] = true;
+  console.dir(wordObject);
+  return {msg: 'Thanks for submitting ' + word + '!'};
 };
 
-app.get("/adjective", function(req, res) {
-    res.json(getRandomWord(adjective));
+app.get('/adjective', function(req, res) {
+  res.json(getRandomWord(adjective));
 });
 
-app.get("/", function (req, res) {
-    res.send("hello universe!");
+app.get('/verb', function(req, res) {
+  res.json(getRandomWord(verb));
+});
+
+app.get('/noun', function(req, res) {
+  res.json(getRandomWord(noun));
+});
+
+app.post('/adjective', function(req, res) {
+  var word = postWord(req.body.word, adjective);
+  res.json(word);
+});
+
+app.post('/noun', function(req, res) {
+  var word = postWord(req.body.word, noun);
+  res.json(word);
+});
+
+app.post('/verb', function(req, res) {
+  var word = postWord(req.body.word, verb);
+  res.json(word);
+});
+
+app.get('/', function(req, res) {
+  res.sendFile('index.html');
 });
 
 app.listen(port, function() {
-    console.log('listeing')
-    console.log('Server started on port ' + port);
+  console.log('server starting. available at http://localhost:' + port);
 });
-
-app.use(express.static(__dirname + "/app/"));
